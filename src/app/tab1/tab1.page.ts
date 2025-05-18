09019bc667e9951f8bd69cac873a1b863796ad1b
Fajard15 <092022090220@student.jgu.ac.id>
2025-05-18 23:20:52 +0700

    Initial commit - UTS Ionic project

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,7 +7,14 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab1Page {
+  articles: { title: string; content: string }[] = [];
 
-  constructor() {}
-
+  constructor() {
+    for (let i = 1; i <= 100; i++) {
+      this.articles.push({
+        title: `Article #${i}`,
+        content: `This is the content for article number ${i}.`
+      });
+    }
+  }
 }
