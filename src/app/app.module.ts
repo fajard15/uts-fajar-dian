a68dc15e71192116183d324ce1c62c102a050158
fajard15 <092022090220@student.jgu.ac.id>
2025-05-19 12:23:14 +0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,11 @@
-import { NgModule } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { Tab1Page } from './tab1.page';
 import { ArticleComponent } from '../components/article/article.component';
 
 @NgModule({
-  declarations: [
-    Tab1Page,
-    ArticleComponent,   // deklarasi komponen di sini
-  ],
+  declarations: [Tab1Page],
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ArticleComponent, // import komponen
   ],
 })
-export class Tab1PageModule {}
